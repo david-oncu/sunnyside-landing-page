f29092ec900722cdca5808c589e4af99760614df
David Oncu <davidoncu@gmail.com>
2022-08-01 16:45:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// Navigation Toggle 
+
+const navToggle = document.getElementById("nav-toggle");
+const primaryNav = document.getElementById("primary-navigation");
+
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+       
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
